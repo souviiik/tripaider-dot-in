67e51918630bda71b6cd93f930aecc3cd2e82f39
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2020-07-14 20:30:54 +0530

    modified post project form

--- src/components/PostProject.js
+++ src/components/PostProject.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Helmet } from "react-helmet";
 
 class PostProject extends Component {
-    state = {  }
+    state = {  };
+
     render() { 
         return (
         <div className="container py-5">
@@ -16,37 +17,31 @@ class PostProject extends Component {
             <form>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Choose a name for your project</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
+                    <input type="text" className="form-control" placeholder="Choose a name for your project"/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">From</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>Start city</label>
+                    <input type="text" className="form-control" placeholder="Start city"/>                    
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">Destinantion</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>Destinantion</label>
+                    <input type="text" className="form-control" placeholder="Destinantion"/>                    
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">Start date</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>Start date</label>
+                    <input type="text" className="form-control" placeholder="Start date"/>                    
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">How many days</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>For how many days</label>
+                    <input type="text" className="form-control" placeholder="For how many days"/>                    
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">How many heads</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" placeholder="e.g. Build a website for my retail business"/>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>How many people</label>
+                    <input type="text" className="form-control" placeholder="How many people"/>                    
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">Tell us more about your trip</label>
-                    <textarea aria-describedby="emailHelp" className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Start with the task you need done..."></textarea>
-                    <small id="emailHelp" className="form-text text-muted">Remember to include the goal, the timelines, any special requirements and any other details that could help.</small>
+                    <label>Tell us more about your trip</label>
+                    <textarea className="form-control" rows="3" placeholder="Tell us more about your trip..."></textarea>                    
                 </div>
                 <button type="submit" className="btn btn-primary">Create requirement</button>
             </form>
